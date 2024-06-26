8317453617152cf4c991418ad04f0c3a880d994c
erichiroshi <erichiroshi@gmail.com>
2024-06-26 09:39:56 -0400

    h2, entity Point, seed db Point

---
+++ src/main/java/br/com/erichiroshi/desafio_backend_br_points_of_interest/entity/Point.java
@@ -0,0 +1,28 @@
+package br.com.erichiroshi.desafio_backend_br_points_of_interest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_point")
+public class Point {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private int x;
+	private int y;
+	private String name;
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_point(x, y, name) VALUES (27, 12, 'Lanchonete');
+INSERT INTO tb_point(x, y, name) VALUES (31, 18, 'Posto');
+INSERT INTO tb_point(x, y, name) VALUES (15, 12, 'Joalheria');
+INSERT INTO tb_point(x, y, name) VALUES (19, 21, 'Floricultura');
+INSERT INTO tb_point(x, y, name) VALUES (12, 8, 'Pub');
+INSERT INTO tb_point(x, y, name) VALUES (23, 6, 'Supermercado');
+INSERT INTO tb_point(x, y, name) VALUES (28, 2, 'Churrascaria');
