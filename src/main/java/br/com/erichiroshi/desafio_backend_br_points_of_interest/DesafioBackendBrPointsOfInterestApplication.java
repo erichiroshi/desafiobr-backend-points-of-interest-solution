251c0cb2c5c6d8d8b0ba1ff06cce56203dd0207b
erichiroshi <erichiroshi@gmail.com>
2024-06-26 09:29:43 -0400

    project created

---
+++ src/main/java/br/com/erichiroshi/desafio_backend_br_points_of_interest/DesafioBackendBrPointsOfInterestApplication.java
@@ -0,0 +1,13 @@
+package br.com.erichiroshi.desafio_backend_br_points_of_interest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendBrPointsOfInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendBrPointsOfInterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erichiroshi/desafio_backend_br_points_of_interest/DesafioBackendBrPointsOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erichiroshi.desafio_backend_br_points_of_interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendBrPointsOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
